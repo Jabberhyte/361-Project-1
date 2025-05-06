bc898a801ec47d76f24dfeddc7bf06fb0e27d951
Jabberhyte <pkohlersaurus@gmail.com>
2025-05-05 19:04:25 -0700

    add user creation

--- login.cpp
+++ login.cpp
@@ -4,52 +4,38 @@
 int main() {
     std::string userVerify;
     while(true) {
-        std::ifstream verify;
-        verify.open ("login.txt");
-        verify >> userVerify;
-        verify.close();
-
-        std::ifstream user;
-        user.open("users/" + userVerify + ".txt");
-        
-        std::ofstream submit;
-        submit.open ("login.txt");
-
-        if (user.is_open()) {
-            submit << "success";
-            submit.close();
-            break;
-        } else {
-            submit << "failure";
-            submit.close();
+        std::ifstream verify("login.txt");
+        std::string line, command, data;
+        while (std::getline(verify, line)) {
+            if (line.rfind("COMMAND:", 0) == 0) {
+                size_t colon1 = line.find(':', 8);
+                command = line.substr(8, colon1 - 8);
+                data = line.substr(colon1 + 1);
+            }
         }
-    }
-
-    while(true) {
-        std::ifstream verify;
-        std::string passAttempt;
-
-        verify.open ("login.txt");
-        verify >> passAttempt;
         verify.close();
 
-        std::ifstream user;
-        user.open("users/" + userVerify + ".txt");
-
-        std::ofstream submit;
-        submit.open ("login.txt");
-
-        if (user.is_open()) {
-            std::string pass;
-            user >> pass;
-            if (pass == passAttempt) {
-                submit << "success";
-                submit.close();
-                break;
+        std::ofstream out("login.txt");
+        if (command == "USERNAME") {
+            std::ifstream user("users/" + data + ".txt");
+            if (user.is_open()) {
+                userVerify = data;
+                out << "COMMAND:USERNAME:" << data << "\nRESPONSE:success";
+            } else {
+                out << "COMMAND:USERNAME:" << data << "\nRESPONSE:failure";
+            }
+        } else if (command == "PASSWORD") {
+            std::ifstream user("users/" + userVerify + ".txt");
+            std::string storedPass;
+            user >> storedPass;
+            if (storedPass == data) {
+                out << "COMMAND:PASSWORD:" << data << "\nRESPONSE:success";
+            } else {
+                out << "COMMAND:PASSWORD:" << data << "\nRESPONSE:failure";
             }
         } else {
-            submit << "failure";
-            submit.close();
+            out << "RESPONSE:pending";
         }
+        out.close();
     }
 }
--- main.cpp
+++ main.cpp
@@ -1,5 +1,45 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+bool createUser() {
+    std::string name;
+    std::string password;
+
+    std::cout << "Please enter your prefered name: ";
+    std::cin >> name;
+
+    std::ofstream user("users/" + name + ".txt");
+    
+    std::cout << "Please enter your prefered password.  This password cannot be retrieved later if forgotten: ";
+    std::cin >> password;
+    user << password;
+
+    return true;
+}
+
+void writeSubmission(const std::string& cmd, const std::string& data) {
+    std::ofstream submit("login.txt");
+    submit << "COMMAND:" << cmd << ":" << data << "\nRESPONSE:pending";
+    submit.close();
+}
+
+
+std::string waitForResponse() {
+    std::string line;
+    for (int i = 0; i < 100; ++i) {
+        std::ifstream verify("login.txt");
+        while (std::getline(verify, line)) {
+            if (line.rfind("RESPONSE:", 0) == 0) {
+                std::string result = line.substr(9);
+                if (result != "pending") {
+                    return result;
+                }
+            }
+        }
+    }
+    return "timeout";
+}
 
 bool login(int count) {
     std::cout << "Hello!  Please enter 1 if you are a returning user.  If you are a new uaser, please enter 2: ";
@@ -7,22 +47,18 @@ bool login(int count) {
     int initial;
     std::cin >> initial;
 
-    if (initial == 1) {
-        std::string username;
-        std::string password;
+    if (initial == 2) {
+        return createUser();
+    } else if (initial == 1) {
+        std::string username, password, userVerify;
 
         std::cout << "Please enter your username: ";
         std::cin >> username;
 
-        std::ofstream submit;
-        submit.open ("login.txt");
-        submit << username;
-        submit.close();
+        
+        writeSubmission("USERNAME", username);
 
-        std::ifstream verify;
-        std::string userVerify;
-        verify.open ("login.txt");
-        verify >> userVerify;
+        userVerify = waitForResponse();
 
         if (userVerify == "failure") {
             std::cout << "Can't find user of that name" << std::endl;
@@ -37,13 +73,9 @@ bool login(int count) {
         std::cout << "Please enter your password: ";
         std::cin >> password;
 
+        writeSubmission("PASSWORD", password);
 
-        submit.open ("login.txt");
-        submit << password;
-        submit.close();
-
-        verify.open ("login.txt");
-        verify >> userVerify;
+        userVerify = waitForResponse();
 
         if (userVerify == "failure") {
             std::cout << "Can't find user of that name" << std::endl;
@@ -55,6 +87,9 @@ bool login(int count) {
             }
         } else {
             std::cout << "login successful" << std::endl;
+
+            writeSubmission("FINISHED", "finished");
+
             return true;
         }
     }
