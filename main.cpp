1cdb5400684d9c471f4d9cfd6b21aed766a68c5f
Jabberhyte <pkohlersaurus@gmail.com>
2025-05-05 17:53:16 -0700

    add main logic for login verification

---
+++ main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+
+int main() {
+    bool loginAttempt = login(0);
+}
+
+bool login(int count) {
+    std::cout << "Hello!  Please enter 1 if you are a returning user.  If you are a new uaser, please enter 2: ";
+
+    int initial;
+    std::cin >> initial;
+
+    if (initial == 1) {
+        std::string username;
+        std::string password;
+
+        std::cout << "Please enter your username: ";
+        std::cin >> username;
+
+        std::ofstream submit;
+        submit.open ("login.txt");
+        submit << username;
+        submit.close();
+
+        std::ifstream verify;
+        std::string userVerify;
+        verify.open ("login.txt");
+        verify >> userVerify;
+
+        if (userVerify == "failure") {
+            std::cout << "Can't find user of that name" << std::endl;
+            count += 1;
+            if (count < 3) {
+                login(count);
+            } else {
+                return false;
+            }
+        }
+
+        std::cout << "Please enter your password: ";
+        std::cin >> password;
+
+        std::ofstream submit;
+        submit.open ("login.txt");
+        submit << password;
+        submit.close();
+
+        std::ifstream verify;
+        verify.open ("login.txt");
+        verify >> userVerify;
+
+        if (userVerify == "failure") {
+            std::cout << "Can't find user of that name" << std::endl;
+            count += 1;
+            if (count < 3) {
+                login(count);
+            } else {
+                return false;
+            }
+        }
+    }
+
+}
